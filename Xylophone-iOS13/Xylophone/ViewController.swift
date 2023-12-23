311f1cf9b4887026e7b3c03c02382d35f6bccc5c
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-12-23 15:29:55 +0900

    :white_check_mark: Done section 7

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func onPressed(_ sender: UIButton) {
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+                UIView.animate(withDuration: 0.2) {
+                    sender.alpha = 1.0
+                }
+            }
+        playSound(value : sender.currentTitle!)
+    }
+    
+    func playSound(value: String) {
+        let url = Bundle.main.url(forResource: value, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    // challenge
 }
-
