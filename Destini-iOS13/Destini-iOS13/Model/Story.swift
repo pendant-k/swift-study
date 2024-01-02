623000ea6ac5e942c0ea33d249994587c4981fbe
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-01-03 00:36:51 +0900

    :white_check_mark: Done section 10 project

--- Destini-iOS13/Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Destini-iOS13/Model/Story.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Story {
     let title: String
     let choice1: String
-    let choice1Destination: String
+    let choice1Destination: Int
     let choice2: String
-    let choice2Destination: String
+    let choice2Destination: Int
 }
