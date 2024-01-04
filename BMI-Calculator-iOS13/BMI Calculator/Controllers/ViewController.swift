779281fe8b8ae5ab32380ef087c2b53925a6160b
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-01-04 12:22:49 +0900

    :construction: Learning navigation on IOS

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var heightLabel: UILabel!
+    
+    @IBOutlet var heightSlider: UISlider!
+    @IBOutlet var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(Int(sender.value))
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    // IBOutlet을 통해서 다른 UI의 value값을 쉽게 가져올 수 있다
+    @IBAction func caculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = String(format: "%.2f", weight / pow(height, 2))
+        
+        //        print(bmi)
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = bmi
+        
+        // present - modal 형태로 View controller를 띄우는 것
+        //        present(secondVC, animated: true, completion: nil)
+    }
 }
-
