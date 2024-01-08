abb5db6ccaab83f052a8eca7bd850e26c2710f37
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-01-08 19:03:36 +0900

    :sparkles: Implement tipsy project

--- Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -9,25 +9,80 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    var selectedTip: Float = 1.0
+    var splitedTO: Int = 2
+    var calculated: Float = 0.0
+
+    func resetUI() {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+    }
+
     @IBOutlet var tenPctButton: UIButton!
     @IBOutlet var zeroPctButton: UIButton!
     @IBOutlet var twentyPctButton: UIButton!
     @IBOutlet var billTextField: UITextField!
     @IBOutlet var splitNumberLabel: UILabel!
 
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        resetUI()
+        let pressedTitle = sender.titleLabel!.text!
+        switch pressedTitle {
+        case "0%":
+            zeroPctButton.isSelected = true
+            selectedTip = 0.0
+        case "10%":
+            tenPctButton.isSelected = true
+            selectedTip = 1.0
+        case "20%":
+            twentyPctButton.isSelected = true
+            selectedTip = 2.0
+        default:
+            print("error")
+        }
+    }
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        splitedTO = Int(sender.value)
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let userInput = Float(billTextField.text!)!
+
+        calculated = (userInput + (userInput * selectedTip)) / Float(splitedTO)
+
+        performSegue(withIdentifier: "goToResults", sender: self)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetUI()
 
         // Do any additional setup after loading the view.
     }
 
-    /*
-     // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var returnText = ""
+        switch selectedTip {
+        case 0.0:
+            returnText = "0%"
+        case 1.0:
+            returnText = "10%"
+        case 2.0:
+            returnText = "20%"
+        default:
+            returnText = "0%"
+        }
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
+        if segue.identifier == "goToResults" {
+            print("it works")
+            let destinationVC = segue.destination as! ResultsViewController
+
+            destinationVC.settingsText = "Split between \(splitedTO) people, with \(returnText)"
+            destinationVC.totalText = String(format: "%.2f", calculated)
+        }
+    }
 }
--- Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    var totalText: String?
+    var settingsText: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if totalText != nil {
+            totalLabel.text = totalText
+        }
+        if settingsText != nil {
+            settingsLabel.text = settingsText
+        }
+
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var settingsLabel: UILabel!
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
+    /*
+     // MARK: - Navigation
 
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
 }
