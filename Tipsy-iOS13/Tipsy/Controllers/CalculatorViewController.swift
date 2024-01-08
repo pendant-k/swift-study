e9d7c17081eb099cede247f87ea8f43b3d8e7795
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-01-08 17:55:04 +0900

    :white_check_mark: Tipsy/add IBOutlets

---
+++ Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CalculatorViewController.swift
+//  Tipsy
+//
+//  Created by Donghan Kim on 1/8/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet var tenPctButton: UIButton!
+    @IBOutlet var zeroPctButton: UIButton!
+    @IBOutlet var twentyPctButton: UIButton!
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var splitNumberLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
